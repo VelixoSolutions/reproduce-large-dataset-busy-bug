4295d52724f7b8fa5ed2de91a6d134513eadb821
Pavel Kabir <pkabir.work@gmail.com>
2022-08-23 10:36:54 +0100

    Initial commit

---
+++ src/functions/functions.ts
@@ -0,0 +1,9 @@
+﻿/**
+ * Returns 42
+ * @customfunction ANSWER
+ * @returns String to write.
+ */
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export function answer(): string[][] {
+  return Array(100000).fill(Array(12).fill("test-result"));
+}
